8becb6046e95b9920dffdd870c69c6e552c8dffe
Warvito <ianonimato@hotmail.com>
2015-12-05 21:32:00 +0000

    Optimizers

--- codes/mlp.py
+++ codes/mlp.py
@@ -1,5 +1,3 @@
-__docformat__ = 'restructedtext en'
-
 import os
 import sys
 import timeit
@@ -9,82 +7,144 @@ import numpy
 import theano
 import theano.tensor as T
 
+from theano.tensor.shared_randomstreams import RandomStreams
 
-from logistic_sgd import LogisticRegression, load_data
+from logistic_sgd import LogisticRegression
 
-class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
-                 activation=T.tanh):
-        self.input = input
-        if W is None:
-            W_values = numpy.asarray(
-                rng.uniform(
-                    low=-numpy.sqrt(6. / (n_in + n_out)),
-                    high=numpy.sqrt(6. / (n_in + n_out)),
-                    size=(n_in, n_out)
-                ),
-                dtype=theano.config.floatX
-            )
-            if activation == theano.tensor.nnet.sigmoid:
-                W_values *= 4
+from utils import load_data, tile_raster_images
 
-            W = theano.shared(value=W_values, name='W', borrow=True)
+from toy_dataset import toy_dataset
 
-        if b is None:
-            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
-            b = theano.shared(value=b_values, name='b', borrow=True)
-
-        self.W = W
-        self.b = b
-
-        lin_output = T.dot(input, self.W) + self.b
-        self.output = (
-            lin_output if activation is None
-            else activation(lin_output)
-        )
-        self.params = [self.W, self.b]
+from mlp import HiddenLayer
 
 class MLP(object):
 
-    def __init__(self, rng, input, n_in, n_hidden, n_out):
+    def __init__(self,
+                 n_ins=784,
+                 hidden_layers_sizes=[500, 500],
+                 n_outs=10,
+                 numpy_rng=None,
+                 theano_rng=None):
+        
+        self.sigmoid_layers = []
+        self.rbm_layers = []
+        self.params = []
+        self.n_layers = len(hidden_layers_sizes)
 
-        self.hiddenLayer = HiddenLayer(
-            rng=rng,
-            input=input,
-            n_in=n_in,
-            n_out=n_hidden,
-            activation=T.tanh
+        if numpy_rng is None:
+            numpy_rng = numpy.random.RandomState(1234)
+
+        if theano_rng is None:
+            theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
+
+        self.x = T.matrix('x')  
+        self.y = T.ivector('y')  
+
+        for i in xrange(self.n_layers):
+            if i == 0:
+                input_size = n_ins
+            else:
+                input_size = hidden_layers_sizes[i - 1]
+
+            if i == 0:
+                layer_input = self.x
+            else:
+                layer_input = self.sigmoid_layers[-1].output
+
+            sigmoid_layer = HiddenLayer(rng=numpy_rng,
+                                        input=layer_input,
+                                        n_in=input_size,
+                                        n_out=hidden_layers_sizes[i],
+                                        activation=T.nnet.sigmoid)
+
+            self.sigmoid_layers.append(sigmoid_layer)
+
+            self.params.extend(sigmoid_layer.params)
+
+        self.logLayer = LogisticRegression(
+            input=self.sigmoid_layers[-1].output,
+            n_in=hidden_layers_sizes[-1],
+            n_out=n_outs)
+        self.params.extend(self.logLayer.params)
+
+        self.finetune_cost = self.logLayer.negative_log_likelihood(self.y)
+
+        self.errors = self.logLayer.errors(self.y)
+
+
+    def build_finetune_functions(self, datasets, batch_size, learning_rate):
+
+        (train_set_x, train_set_y) = datasets[0]
+        (valid_set_x, valid_set_y) = datasets[1]
+        (test_set_x, test_set_y) = datasets[2]
+
+        n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+        n_valid_batches /= batch_size
+        n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+        n_test_batches /= batch_size
+
+        index = T.lscalar('index') 
+
+        gparams = T.grad(self.finetune_cost, self.params)
+
+        updates = []
+        for param, gparam in zip(self.params, gparams):
+            updates.append((param, param - gparam * learning_rate))
+
+        train_fn = theano.function(
+            inputs=[index],
+            outputs=self.finetune_cost,
+            updates=updates,
+            givens={
+                self.x: train_set_x[
+                    index * batch_size: (index + 1) * batch_size
+                ],
+                self.y: train_set_y[
+                    index * batch_size: (index + 1) * batch_size
+                ]
+            }
         )
 
-        self.logRegressionLayer = LogisticRegression(
-            input=self.hiddenLayer.output,
-            n_in=n_hidden,
-            n_out=n_out
+        test_score_i = theano.function(
+            [index],
+            self.errors,
+            givens={
+                self.x: test_set_x[
+                    index * batch_size: (index + 1) * batch_size
+                ],
+                self.y: test_set_y[
+                    index * batch_size: (index + 1) * batch_size
+                ]
+            }
         )
 
-        self.L1 = (
-            abs(self.hiddenLayer.W).sum()
-            + abs(self.logRegressionLayer.W).sum()
+        valid_score_i = theano.function(
+            [index],
+            self.errors,
+            givens={
+                self.x: valid_set_x[
+                    index * batch_size: (index + 1) * batch_size
+                ],
+                self.y: valid_set_y[
+                    index * batch_size: (index + 1) * batch_size
+                ]
+            }
         )
 
-        self.L2_sqr = (
-            (self.hiddenLayer.W ** 2).sum()
-            + (self.logRegressionLayer.W ** 2).sum()
-        )
+        def valid_score():
+            return [valid_score_i(i) for i in xrange(n_valid_batches)]
 
-        self.negative_log_likelihood = (
-            self.logRegressionLayer.negative_log_likelihood
-        )
-        self.errors = self.logRegressionLayer.errors
+        def test_score():
+            return [test_score_i(i) for i in xrange(n_test_batches)]
 
-        self.params = self.hiddenLayer.params + self.logRegressionLayer.params
-
-        self.input = input
+        return train_fn, valid_score, test_score
 
 
-def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
-             dataset='mnist.pkl.gz', batch_size=20, n_hidden=500):
-
+def test_mnist(finetune_lr=0.1,
+             training_epochs=100,
+             dataset='../datasets/mnist.pkl.gz',
+             batch_size=10):
+    
     datasets = load_data(dataset)
 
     train_set_x, train_set_y = datasets[0]
@@ -92,97 +152,51 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
     test_set_x, test_set_y = datasets[2]
 
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
-    n_test_batches = test_set_x.get_value(borrow=True).shape[0] / batch_size
 
+    numpy_rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(numpy_rng.randint(2 ** 30)) 
+    
     print '... building the model'
+    mlp = MLP(numpy_rng=numpy_rng,
+              theano_rng=theano_rng,
+              n_ins=28 * 28,
+              hidden_layers_sizes=[1000],
+              n_outs=10)
 
-    index = T.lscalar()  
-    x = T.matrix('x')  
-    y = T.ivector('y') 
-
-    rng = numpy.random.RandomState(1234)
-
-    classifier = MLP(
-        rng=rng,
-        input=x,
-        n_in=28 * 28,
-        n_hidden=n_hidden,
-        n_out=10
+    print '... getting the finetuning functions'
+    train_fn, validate_model, test_model = mlp.build_finetune_functions(
+        datasets=datasets,
+        batch_size=batch_size,
+        learning_rate=finetune_lr
     )
 
-    cost = (
-        classifier.negative_log_likelihood(y)
-        + L1_reg * classifier.L1
-        + L2_reg * classifier.L2_sqr
-    )
-
-    test_model = theano.function(
-        inputs=[index],
-        outputs=classifier.errors(y),
-        givens={
-            x: test_set_x[index * batch_size:(index + 1) * batch_size],
-            y: test_set_y[index * batch_size:(index + 1) * batch_size]
-        }
-    )
-
-    validate_model = theano.function(
-        inputs=[index],
-        outputs=classifier.errors(y),
-        givens={
-            x: valid_set_x[index * batch_size:(index + 1) * batch_size],
-            y: valid_set_y[index * batch_size:(index + 1) * batch_size]
-        }
-    )
-
-    gparams = [T.grad(cost, param) for param in classifier.params]
-
-    updates = [
-        (param, param - learning_rate * gparam)
-        for param, gparam in zip(classifier.params, gparams)
-    ]
-
-    train_model = theano.function(
-        inputs=[index],
-        outputs=cost,
-        updates=updates,
-        givens={
-            x: train_set_x[index * batch_size: (index + 1) * batch_size],
-            y: train_set_y[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    print '... training'
-
-
-    patience = 10000  
-    patience_increase = 2  
+    print '... finetuning the model'
+    patience = 4 * n_train_batches  
+    patience_increase = 2.    
     improvement_threshold = 0.995  
     validation_frequency = min(n_train_batches, patience / 2)
-                                  
+
     best_validation_loss = numpy.inf
-    best_iter = 0
     test_score = 0.
     start_time = timeit.default_timer()
 
-    epoch = 0
     done_looping = False
+    epoch = 0
 
-    while (epoch < n_epochs) and (not done_looping):
+    while (epoch < training_epochs) and (not done_looping):
         epoch = epoch + 1
         for minibatch_index in xrange(n_train_batches):
 
-            minibatch_avg_cost = train_model(minibatch_index)
+            minibatch_avg_cost = train_fn(minibatch_index)
             iter = (epoch - 1) * n_train_batches + minibatch_index
 
             if (iter + 1) % validation_frequency == 0:
-                validation_losses = [validate_model(i) for i
-                                     in xrange(n_valid_batches)]
-                this_validation_loss = numpy.mean(validation_losses)
 
+                validation_losses = validate_model()
+                this_validation_loss = numpy.mean(validation_losses)
                 print(
-                    'epoch %i, minibatch %i/%i, validation error %f %%' %
-                    (
+                    'epoch %i, minibatch %i/%i, validation error %f %%'
+                    % (
                         epoch,
                         minibatch_index + 1,
                         n_train_batches,
@@ -191,6 +205,7 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
                 )
 
                 if this_validation_loss < best_validation_loss:
+
                     if (
                         this_validation_loss < best_validation_loss *
                         improvement_threshold
@@ -200,10 +215,8 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
                     best_validation_loss = this_validation_loss
                     best_iter = iter
 
-                    test_losses = [test_model(i) for i
-                                   in xrange(n_test_batches)]
+                    test_losses = test_model()
                     test_score = numpy.mean(test_losses)
-
                     print(('     epoch %i, minibatch %i/%i, test error of '
                            'best model %f %%') %
                           (epoch, minibatch_index + 1, n_train_batches,
@@ -214,13 +227,18 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
                 break
 
     end_time = timeit.default_timer()
-    print(('Optimization complete. Best validation score of %f %% '
-           'obtained at iteration %i, with test performance %f %%') %
-          (best_validation_loss * 100., best_iter + 1, test_score * 100.))
-    print >> sys.stderr, ('The code for file ' +
+    print(
+        (
+            'Optimization complete with best validation score of %f %%, '
+            'obtained at iteration %i, '
+            'with test performance %f %%'
+        ) % (best_validation_loss * 100., best_iter + 1, test_score * 100.)
+    )
+    print >> sys.stderr, ('The fine tuning code for file ' +
                           os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
-
+                          ' ran for %.2fm' % ((end_time - start_time)
+                                              / 60.))
 
 if __name__ == '__main__':
-    test_mlp()
+    test_mnist()
+
