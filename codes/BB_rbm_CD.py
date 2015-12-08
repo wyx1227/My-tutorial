0aeda6d3d382dbb12caf0e7c0cbc3004dce8e8db
Warvito <ianonimato@hotmail.com>
2015-12-08 20:49:07 +0000

    Another day

--- codes/BB_rbm_CD.py
+++ codes/BB_rbm_CD.py
@@ -291,11 +291,14 @@ def test_toy(learning_rate=0.1,
     os.chdir('../')             
                  
                  
-def test_mnist(learning_rate=0.1, training_epochs=15,
-             dataset='../datasets/mnist.pkl.gz', batch_size=20,
-             n_chains=20, n_samples=10, 
-             output_folder='MNIST_rbm_CD_plots',
-             n_hidden=500):
+def test_mnist(learning_rate=0.1,
+               training_epochs=15, 
+               dataset='../datasets/mnist.pkl.gz', 
+               batch_size=20, 
+               n_chains=20,
+               n_samples=10, 
+               output_folder='MNIST_rbm_CD_plots',
+               n_hidden=500):
 
     print 'Loading MNIST dataset...'
     datasets = load_data(dataset)
@@ -415,8 +418,139 @@ def test_mnist(learning_rate=0.1, training_epochs=15,
     image = Image.fromarray(image_data)
     image.save('samples.png')
     os.chdir('../')      
+              
+              
+          
+def test_mnist_14x14(learning_rate=0.1,
+                     training_epochs=15, 
+                     dataset='../datasets/mnist_14x14.pkl.gz', 
+                     batch_size=10, 
+                     n_chains=20,
+                     n_samples=10, 
+                     output_folder='MNIST_14x14_rbm_CD_plots',
+                     n_hidden=200):
+
+    print 'Loading MNIST 14x14 dataset...'
+    datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets[0]
+    test_set_x, test_set_y = datasets[2]
+
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+
+    index = T.lscalar()  
+    x = T.matrix('x')
+
+    rng = numpy.random.RandomState(123)
+    theano_rng = RandomStreams(rng.randint(2 ** 30))
+    
+    print 'Creating RBM...'
+    rbm = RBM(input=x, n_visible=14 * 14,
+              n_hidden=n_hidden, numpy_rng=rng, theano_rng=theano_rng)
+
+    cost, updates = rbm.get_cost_updates(lr=learning_rate, k=15)
+
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    os.chdir(output_folder)
+
+    train_rbm = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size]
+        },
+        name='train_rbm'
+    )
+    print 'Starting training with %d epochs' %training_epochs
+    plotting_time = 0.
+    start_time = timeit.default_timer()
+
+    for epoch in xrange(training_epochs):
+        mean_cost = []
+        for batch_index in xrange(n_train_batches):
+            mean_cost += [train_rbm(batch_index)]
+
+        print 'Training epoch %d, cost is ' % epoch, numpy.mean(mean_cost)
+
+        plotting_start = timeit.default_timer()
+        image = Image.fromarray(
+            tile_raster_images(
+                X=rbm.W.get_value(borrow=True).T,
+                img_shape=(14, 14),
+                tile_shape=(10, 10),
+                tile_spacing=(1, 1)
+            )
+        )
+        image.save('filters_at_epoch_%i.png' % epoch)
+        plotting_stop = timeit.default_timer()
+        plotting_time += (plotting_stop - plotting_start)
+
+    end_time = timeit.default_timer()
+
+    pretraining_time = (end_time - start_time) - plotting_time
+    print ('Training took %.2f minutes' % (pretraining_time / 60.))
+
+    number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
+
+    test_idx = rng.randint(number_of_test_samples - n_chains)
+    persistent_vis_chain = theano.shared(
+        numpy.asarray(
+            test_set_x.get_value(borrow=True)[test_idx:test_idx + n_chains],
+            dtype=theano.config.floatX
+        )
+    )
+
+    plot_every = 1000
+    (
+        [
+            presig_hids,
+            hid_mfs,
+            hid_samples,
+            presig_vis,
+            vis_mfs,
+            vis_samples
+        ],
+        updates
+    ) = theano.scan(
+        rbm.gibbs_vhv,
+        outputs_info=[None, None, None, None, None, persistent_vis_chain],
+        n_steps=plot_every
+    )
+
+    updates.update({persistent_vis_chain: vis_samples[-1]})
+    sample_fn = theano.function(
+        [],
+        [
+            vis_mfs[-1],
+            vis_samples[-1]
+        ],
+        updates=updates,
+        name='sample_fn'
+    )
+
+    image_data = numpy.zeros(
+        (15 * n_samples + 1, 15 * n_chains - 1),
+        dtype='uint8'
+    )
+    for idx in xrange(n_samples):
+        vis_mf, vis_sample = sample_fn()
+        print ' ... plotting sample ', idx
+        image_data[15 * idx:15 * idx + 14, :] = tile_raster_images(
+            X=vis_mf,
+            img_shape=(14, 14),
+            tile_shape=(1, n_chains),
+            tile_spacing=(1, 1)
+        )
+
+
+    image = Image.fromarray(image_data)
+    image.save('samples.png')
+    os.chdir('../')      
                  
 
 if __name__ == '__main__':
     #test_mnist()
+    test_mnist_14x14()
     test_toy()
